beb702b2827fb9c8b8704cc4e8ed361aa17655ea
Felix Bartels <felix@host-consultants.de>
2020-06-26 23:44:02 +0200

    there can only be one exec in the startup script
    reduce layers
    
    make vault init start delayed as vault need to be online for it to succeed
    
    update postinstall
    
    do not store keys on disk but only give them to stdout
    
    add ldap config to extra script
    as the vault cli needs to authenticate with the root token this can not be added to the startup script
    
    add oidc config template
    
    since its single process, use gosu instead of supervisorctl
    incorporate changes from https://github.com/euanmcgregor/vault-cloudron/pull/2/
    
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

---
+++ ldap-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+set -x
+
+# configure ldap
+
+/app/code/vault login
+/app/code/vault auth enable ldap
+/app/code/vault write auth/ldap/config \
+  url="${CLOUDRON_LDAP_URL}" \
+  userdn="${CLOUDRON_LDAP_USERS_BASE_DN}" \
+  discoverdn=true \
+  groupdn="${CLOUDRON_LDAP_GROUPS_BASE_DN}" \
+  binddn="${CLOUDRON_LDAP_BIND_DN}" \
+  bindpass="${CLOUDRON_LDAP_BIND_PASSWORD}"
